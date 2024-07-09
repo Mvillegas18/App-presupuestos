d0be91f304d3079bf19adfffe74f226569ac3b08
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-08 22:22:21 -0500

    Add formatCurrency function to format numbers as USD currency

--- src/components/AmountDisplay.tsx
+++ src/components/AmountDisplay.tsx
@@ -1,3 +1,5 @@
+import { formatCurrency } from '../helpers';
+
 type AmountDisplayProps = {
 	label: string;
 	amount: number;
@@ -6,7 +8,8 @@ type AmountDisplayProps = {
 export default function AmountDisplay({ label, amount }: AmountDisplayProps) {
 	return (
 		<p className='text-2xl text-blue-600 font-bold'>
-			{label}: <span className='font-black text-gray-950'>{amount}</span>
+			{label}:{' '}
+			<span className='font-black text-gray-950'>{formatCurrency(amount)}</span>
 		</p>
 	);
 }
