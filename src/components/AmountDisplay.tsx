c6d6a5df0744233641e1051a0dc082265cca1a48
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-08 22:15:36 -0500

    refactor: Add BudgetTracker component with AmountDisplay and reset button

---
+++ src/components/AmountDisplay.tsx
@@ -0,0 +1,12 @@
+type AmountDisplayProps = {
+	label: string;
+	amount: number;
+};
+
+export default function AmountDisplay({ label, amount }: AmountDisplayProps) {
+	return (
+		<p className='text-2xl text-blue-600 font-bold'>
+			{label}: <span className='font-black text-gray-950'>{amount}</span>
+		</p>
+	);
+}
--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,3 +1,33 @@
+import AmountDisplay from './AmountDisplay';
+
 export default function BudgetTracker() {
-	return <div>BudgetTracker</div>;
+	return (
+		<div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+			<div className='flex justify-center'>
+				<img
+					src='/grafico.jpg'
+					alt='Grafico que identifica el porcentaje de los gastos'
+				/>
+			</div>
+
+			<div className='flex flex-col justify-center items-center gap-8'>
+				<button className='bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg'>
+					Resetear App
+				</button>
+
+				<AmountDisplay
+					label='Presupuesto'
+					amount={300}
+				/>
+				<AmountDisplay
+					label='Disponible'
+					amount={200}
+				/>
+				<AmountDisplay
+					label='Gastado'
+					amount={100}
+				/>
+			</div>
+		</div>
+	);
 }
