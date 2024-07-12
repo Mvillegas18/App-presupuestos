20391f9ecf8f4e9eea9c284700a07d7050ed19ea
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-12 12:54:29 -0500

    refactor: Update BudgetTracker component to calculate total expenses and remaining budget dynamically

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,6 +1,17 @@
+import { useMemo } from 'react';
+import { useBudget } from '../hooks/useBudget';
 import AmountDisplay from './AmountDisplay';
 
 export default function BudgetTracker() {
+	const { state } = useBudget();
+
+	const totalExpense = useMemo(
+		() => state.expenses.reduce((total, expense) => expense.amount + total, 0),
+		[state.expenses]
+	);
+
+	const reminingBudget = state.budget - totalExpense;
+
 	return (
 		<div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
 			<div className='flex justify-center'>
@@ -17,15 +28,15 @@ export default function BudgetTracker() {
 
 				<AmountDisplay
 					label='Presupuesto'
-					amount={300}
+					amount={state.budget}
 				/>
 				<AmountDisplay
 					label='Disponible'
-					amount={200}
+					amount={reminingBudget}
 				/>
 				<AmountDisplay
 					label='Gastado'
-					amount={100}
+					amount={totalExpense}
 				/>
 			</div>
 		</div>
